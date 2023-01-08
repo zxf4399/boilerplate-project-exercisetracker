d6e3206def89bc8911256a04d8582f7082a098cd
zxf4399 <zxf4399@gmail.com>
2023-01-08 16:49:30 +0800

    refactor: adjust code struct

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const cors = require("cors");
+const express = require("express");
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+const app = express();
+
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-
-
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+const listener = app.listen(3000, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`);
+});
